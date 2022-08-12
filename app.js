e9b5874fce517c3263a2814120c7de8a0453a7d9
Luis Paz y Miño <luis.phi161@gmail.com>
2022-08-12 16:10:35 -0500

    primero

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import path from "path"
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT;
+
+app.set('view engine', 'hbs');
+
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Luis',
+    titulo: 'Curso de node'
+  })
+})
+
+app.get('/generic', (req, res) =>{
+    res.sendFile(path.resolve() +'/public/generic.html')
+});
+
+
+//Para una ruta no encontrada
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve() +'/public/404.html')
+});
+
+app.listen(port, ()=>{
+    console.log(`Aplicación corriendo en ${port}`);
+});
